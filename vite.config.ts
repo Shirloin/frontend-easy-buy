9e31262f0a7b8c17787a3e667182b58703ec922b
Shirloin <riccardo@binus.ac.id>
2024-09-07 23:35:11 +0700

    fix: vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const wsTarget = process.env.VITE_SOCKET_URL;
 export default defineConfig({
   base: "/",
   plugins: [
@@ -10,14 +11,21 @@ export default defineConfig({
     outDir: 'dist',
     sourcemap: true,
   },
-  // preview: {
-  //   port: 7654,
-  //   strictPort: true,
-  // },
-  // server: {
-  //   port: 7654,
-  //   strictPort: true,
-  //   host: true,
-  //   origin: "http://0.0.0.0:7654",
-  // },
+  preview: {
+    port: 7654,
+    strictPort: true,
+  },
+  server: {
+    port: 7654,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:7654",
+    proxy: {
+      '/socket.io': {
+        target: wsTarget,
+        ws: true,
+        changeOrigin: true,
+      },
+    },
+  },
 })
