aaf80cf75b7ee4547f0644bd6991909ad1ed8fd9
shirloin <riccardo8902@gmail.com>
2025-11-05 22:20:02 +0700

    fix: nginx configuration

--- src/util/Socket.ts
+++ src/util/Socket.ts
@@ -1,5 +1,5 @@
 import socketIO from 'socket.io-client';
-const URL = import.meta.env.VITE_NODE_ENV === 'production' ? import.meta.env.VITE_SOCKET_URL ?? "" : 'https://easy-buy-api.shirloin.my.id';
+const URL = import.meta.env.VITE_NODE_ENV === 'production' ? import.meta.env.VITE_SOCKET_URL ?? "" : 'http://localhost:3000';
 export const socket = socketIO(URL, {
     transports: ["websocket"],
     reconnection: true,
